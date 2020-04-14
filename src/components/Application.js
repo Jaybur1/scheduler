5b4fde02fd06f74ae085d4ea20a5d6f52c4555d6
Jaybur1 <jayburbyga@gmail.com>
2020-04-14 19:39:13 -0400

    initiated effect to use axios to fetch days data from the server

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
-
+import React, { useState,useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { DAYS, APPOINTMENTS } from "constants.js";
+import { APPOINTMENTS } from "constants.js";
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {
+      return setDays([...response.data]);
+    }).catch(err => console.error(err));
+  
+  })
 
   return (
     <main className="layout">
@@ -18,7 +27,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={DAYS} value={day} onChange={setDay} />
+          <DayList days={days} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
