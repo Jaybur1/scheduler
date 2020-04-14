dcaf12158377fbe7b1b498de120114e7f8cd3103
Jaybur1 <jayburbyga@gmail.com>
2020-04-14 15:33:03 -0400

    add appointments rendering

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,33 +1,12 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-import Appointment from 'components/Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-
+import Appointment from "components/Appointment";
+import { DAYS, APPOINTMENTS } from "constants.js";
 
 export default function Application(props) {
-
-  const [day, setDay] = useState("Monday")
-
+  const [day, setDay] = useState("Monday");
 
   return (
     <main className="layout">
@@ -39,7 +18,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={DAYS} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -48,7 +27,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {APPOINTMENTS.map(appointment => (
+          <Appointment key={appointment.id} {...appointment}/>
+        ))}
+          <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
---
+++ src/constants.js
@@ -0,0 +1,65 @@
+
+export const DAYS = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0
+  }
+];
+
+export const APPOINTMENTS = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png"
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  },
+  {
+    id:4,
+    time:"3pm"
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    }
+  }
+];
