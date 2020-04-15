f28cbba96d064be6f0e2eadcd01c38138d360037
Jaybur1 <jayburbyga@gmail.com>
2020-04-15 18:35:38 -0400

    add getInterviewersForDay selector + tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -31,10 +31,10 @@ export default function Application(props) {
     });
   }, []);
 
-  const { day, days, interviewers } = state;
+  const { day, days } = state;
   const appointments = getAppointmentsForDay(state, day).map(appointment => {
     const interview = getInterview(state,appointment.interview);
-    return <Appointment key={appointment.id} {...appointment} interview={interview} interviewers={Object.values(interviewers)}/>;
+    return <Appointment key={appointment.id} {...appointment} interview={interview} interviewers={getInterviewersForDay(state,day)}/>;
   });
 
   return (
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,3 +12,10 @@ export const getInterview = (state, interview) => {
   return { student, interviewer: state.interviewers[interviewer] };
 };
 
+export const getInterviewersForDay = (state, day) => {
+  if (state.days.length === 0) return [];
+  const currentDay = state.days.filter(obj => obj.name === day);
+  return currentDay.length === 0
+    ? []
+    : currentDay[0].interviewers.map(appoId => state.interviewers[appoId]);
+}
